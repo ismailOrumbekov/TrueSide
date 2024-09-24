104fb130149cce57351348b314bb4c7c5be4ea14
ismailOrumbekov <ismailorumbekovv@gmail.com>
2024-09-25 00:19:57 +0500

    builded login page and authorization for user

--- TrueSide/mainApp/models.py
+++ TrueSide/mainApp/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    Account = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    Email = models.CharField(max_length=50, primary_key=True, verbose_name="Почта")
+
+
+class Post(models.Model):
+    Author = models.ForeignKey(Student, on_delete=models.CASCADE, null=True)
+    Title = models.CharField(max_length=100, verbose_name="Заголовок")
+    Description = models.CharField(max_length=500, verbose_name="Описание")
+
+
+
--- TrueSide/mainApp/views.py
+++ TrueSide/mainApp/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
 
-# Create your views here.
+# def login_page(request):
+#     return render(request, "Pages/loginPage.html")
+
+def registration_page(request):
+    return render(request, "Pages/RegistrationPage.html")
+
+from django.contrib.auth import authenticate, login
+
+def login_page(request):
+    print("entered")
+    if request.method == 'POST':
+        print("posteed")
+        username = request.POST['username']
+        password = request.POST['password']
+        # Изменяем вызов authenticate на именованные параметры
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('register')
+        else:
+            return redirect('login')
+
+    return render(request, "Pages/loginPage.html")
+
